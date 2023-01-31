24f4aee8421aab74a85193194e26a87fecd562b9
Daeshaun Willis <ddwillis59@gmail.com>
2023-01-31 00:32:07 -0500

    added serverside interaction

--- client/src/components/routes/Home.jsx
+++ client/src/components/routes/Home.jsx
@@ -1,29 +1,44 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import Nav from "../Nav"
 import Blog from "../Blog"
 import Header from "../Header"
-import blogs from "../../blogs.json"
+
 
 function Home(){
+
+    const[data, setData] = useState(null);
+
+    useEffect(()=>{
+        fetch("/home").then(
+            reponse => reponse.json()
+        ).then(
+            data => {setData(data)}
+        )
+    }, [])
+
+
+
     function createBlogs(blog){
         return(
             <Blog 
+            key = {blog._id}
             title = {blog.title}
-            content = {blog.content}
+            content = {blog.message}
             />
-        )
-        }
-        return(
-        <div>
-            <Nav/>
-            <body>
-            <Header/>
-            {blogs.map(createBlogs)}
-            </body>
-            
-        </div>
+        );
+    };
+
+
+    return(
+    <div>
+        <Nav/>
         
-        )
+        {data ? <body><Header/>{data.map(createBlogs)}</body>: <div className="loading"></div>}
+        
+        
+    </div>
+    
+    )
 }
 
 export default Home;
---
+++ server/index.js
@@ -0,0 +1,32 @@
+
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser: true});
+
+
+const postSchema = mongoose.Schema({
+  title: String,
+  message: String
+});
+
+const Post = mongoose.model("Post",postSchema);
+
+
+app.get("/home", (req, res)=>{
+
+    Post.find({},(err,posts)=>{
+        if(err) return res.status(500).send(err);
+        res.send(posts)
+    });
+});
+
+
+app.listen(3001, ()=>{
+    console.log("Listening")
+})
